41433d0dd84978ae7e4858a932815bf1bcfd6526
Michael Schultz <mikewschultz9029@gmail.com>
2022-10-26 22:01:32 -0400

    started script for expanded binary search - should return desired insertion index if value not found

---
+++ closestOrEqualBinarySearch.js
@@ -0,0 +1,32 @@
+function binarySearch(arr, left, right, val)
+{
+    // arguments:
+    // 1. arr = array of values to search
+    // 2. left = left boundary
+    // 3. right = right boundary
+    // 4. val = value to search for
+    // return:
+    // object containing index and whether the value is found
+    let mid = Math.floor((left + right) / 2)
+    let found = false
+    while (left <= right)
+    {
+        if (arr[mid] === val)
+        {
+            found = true
+            break
+        }
+        if (arr[mid] < val)
+        {
+            left = mid + 1
+        }
+        if (arr[mid] > val)
+        {
+            right = mid - 1
+        }
+        mid = Math.floor((left + right) / 2)
+    }
+    return { index: mid, found: found }
+}
+
+module.exports = binarySearch
