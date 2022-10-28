f57af989d780bfd66359c6f64a7df8985e974f2d
Michael Schultz <mikewschultz9029@gmail.com>
2022-10-27 21:44:52 -0400

    updated comments, added guard clause and conditions to catch out of bounds values before iterating

--- binarySearch.js
+++ binarySearch.js
@@ -1,19 +1,28 @@
 // normal binary search
 function binarySearch(arr, val, options = { left: 0, right: arr.length - 1, order: 'asc' })
 {
-
-    // arguments:
+    /// arguments:
     // 1. arr = array of values to search
-    // 2. left = left boundary
-    // 3. right = right boundary
-    // 4. val = value to search for
+    // 2. val = value to search for
+    // 3. options = object -> keys/values: left: INTEGER, right: INTEGER, order: 'asc' or 'desc'
     // return:
-    // index of value if found, -1 if not found
+    // IF FOUND, index of value
+    // IF NOT FOUND, -1
+    // return value is UNDEFINED if arr is not sorted and/or the provided options.order does not match the actual sorting of arr
 
     // handling assigning default options in case a subset of options is provided
     let left = options.left || 0
     let right = options.right || arr.length - 1
     let order = options.order || 'asc'
+
+    // guard clause to make sure arr has actual content
+    if (arr.length === 0)
+        return -1
+    // out of bounds values return -1
+    if ((val < arr[0] && order == 'asc') || (val > arr[0] && order == 'desc'))
+        return -1
+    if ((val > arr[right] && order == 'asc') || (val < arr[right] && order == 'desc'))
+        return -1
 
     let mid = Math.floor((left + right) / 2)
     while (left <= right)
